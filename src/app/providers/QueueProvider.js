182267a3d0f661e59ff2df0ecf0be169a1e58d03
matheusafonsouza <matheusafonsouza@gmail.com>
2021-06-01 01:17:45 -0300

    fix: job err

--- src/app/providers/QueueProvider.js
+++ src/app/providers/QueueProvider.js
@@ -5,8 +5,9 @@ import RegistrationMail from '../jobs/RegistrationMail';
 
 const mailQueue = new Queue(RegistrationMail.key, redisConfig);
 
-mailQueue.on('failed', (job) => {
+mailQueue.on('failed', (job, err) => {
   console.log(`Job failed ${job.name} - ${job.data}`);
+  console.log(err);
 });
 
 export default mailQueue;
